1fc12938151e6fcee01a8a2accc996feaaa40d7b
alishabab62 <shababali672@gmail.com>
2023-10-19 21:57:36 +0530

    merger sort

---
+++ mereSort.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+void mergeArray(int arr[],int start,int end){
+    int mid = (start+end)/2;
+    int lengthLeft = mid-start+1;
+    int lengthRight = end-mid;
+    int *leftArr = new int[lengthLeft];
+    int *rightArr = new int[lengthRight];
+
+    int k = start;
+    for(int i = 0; i < lengthLeft; i++){
+        leftArr[i] = arr[k];
+        k++;
+    }
+    k = mid+1;
+    for(int i = 0; i < lengthRight; i++){
+        rightArr[i] = arr[k];
+        k++;
+    }
+    int i =0;
+    int j = 0;
+    int indexArr = start;
+    while(i<lengthLeft && j<lengthRight){
+        if(leftArr[i] < rightArr[j]){
+            arr[indexArr] = leftArr[i];
+            indexArr++;
+            i++;
+        }
+        else{
+            arr[indexArr] = rightArr[j];
+            indexArr++;
+            j++;
+        }
+    }
+
+    while(i<lengthLeft){
+        arr[indexArr] = leftArr[i];
+        indexArr++;
+        i++;
+    }
+    while(j<lengthRight){
+        arr[indexArr] = rightArr[j];
+        indexArr++;
+        j++;
+    }
+
+}
+
+void mergeSort(int arr[],int start,int end){
+    if(start >= end){
+        return;
+    }
+    int mid = (start+end)/2;
+    mergeSort(arr,start,mid);
+    mergeSort(arr,mid+1,end);
+    mergeArray(arr,start,end);
+}
+
+
+int main(){
+    int arr[10] = {2,5,3,10,3,6,5,9,0,2};
+    int start = 0;
+    int end = 9;
+    mergeSort(arr,start,end);
+
+    for(int i=start;i<=end;i++){
+        cout << arr[i] << " ";
+    }
+}
